40bab1c96180dc8bb7bbfc0f5bc5818f32bae52d
Omar Gonzalez <gonzalezomar645@gmail.com>
2020-10-31 14:02:47 -0600

    Update/ pulled feature/spotify

--- src/api/routes.js
+++ src/api/routes.js
@@ -2,14 +2,14 @@ const user = require('../components/user/network')
 const auth = require('../components/auth/network')
 const artist = require('../components/artist/network')
 const music = require('../components/music/network')
-// const search = require('../components/search/network')
+const playlist = require('../components/playlist/network')
 
 const routes = app => {
     app.use('/api/user', user)
     app.use('/api/auth', auth)
     app.use('/api/artist', artist)
     app.use('/api/music', music)
-    // app.use('/api/music', music)
+    app.use('/api/playlist', playlist)
 }
 
 module.exports = routes
