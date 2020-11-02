d87d2420366996ae8b2ce0b5a42f57b41f1b4a90
Omar Gonzalez <gonzalezomar645@gmail.com>
2020-11-01 20:46:50 -0600

    Add id to music store

--- src/components/music/store.js
+++ src/components/music/store.js
@@ -2,9 +2,9 @@ const Model = require("../../store/models/playedSongs")
 
 module.exports = {
     async addPlay(play) { return await new Model({ ...play, timeStamp: Date.now() }).save() },
-    async getHistory(user) {
+    async getHistory(id) {
         return await Model.find({
-            user
+            user:id
         })
     }
 }
